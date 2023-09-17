d82b873d4107f097a045c37fcf679511c181f0a5
Tim Kuntz <tim.kuntz@appfolio.com>
2023-09-17 16:28:13 -0500

    Add disabled OpenAI plugin (need paid account)

---
+++ lua/pde/openai.lua
@@ -0,0 +1,30 @@
+-- This requires an OpenAI API key and a pricing plan to work.
+return {
+    "Bryley/neoai.nvim",
+    enabled = false
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    cmd = {
+        "NeoAI",
+        "NeoAIOpen",
+        "NeoAIClose",
+        "NeoAIToggle",
+        "NeoAIContext",
+        "NeoAIContextOpen",
+        "NeoAIContextClose",
+        "NeoAIInject",
+        "NeoAIInjectCode",
+        "NeoAIInjectContext",
+        "NeoAIInjectContextCode",
+    },
+    keys = {
+        { "<leader>as", desc = "summarize text" },
+        { "<leader>ag", desc = "generate git message" },
+    },
+    config = function()
+        require("neoai").setup({
+            -- Options go here
+        })
+    end,
+}
