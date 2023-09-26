0022e11d1260d764221561a29521e9dedc0aa63a
Tim Kuntz <tim.kuntz@appfolio.com>
2023-09-25 22:12:50 -0500

    Disable gitsigns; throwing errors

--- lua/pde/git.lua
+++ lua/pde/git.lua
@@ -16,7 +16,7 @@ return {
 	-- Adds git releated signs to the gutter, as well as utilities for managing changes
 	{
 		"lewis6991/gitsigns.nvim",
-    enabled = false,
+		enabled = false,
 		event = "VeryLazy",
 		enabled = config.pde.git,
 		opts = {
