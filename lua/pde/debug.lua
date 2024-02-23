47faf8581da881f58a3f169f83cbc6c538acf0b7
Tim Kuntz <tim.kuntz@appfolio.com>
2024-02-22 23:26:48 -0600

    Add nvim-dap, keymap an ui configuration, and ruby setup

---
+++ lua/pde/debug.lua
@@ -0,0 +1,110 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "theHamsta/nvim-dap-virtual-text",
+    "rcarriga/nvim-dap-ui",
+    "folke/which-key.nvim",
+  },
+  config = function()
+    require("nvim-dap-virtual-text").setup {
+      commented = true,
+    }
+    -- TODO: Extract this file to a nvim-dap-generic plugin
+    --       removing the language specific bits
+    --       this file will reference the generic plugin
+    --       and add the language specific bits back
+    require("pde.debug.ruby").setup()
+
+    local dap, dapui = require "dap", require "dapui"
+    dap.set_log_level('TRACE')
+
+    dapui.setup {
+      layouts = { {
+        elements = { {
+            id = "scopes",
+            size = 0.25
+          }, {
+            id = "breakpoints",
+            size = 0.25
+          }, {
+            id = "stacks",
+            size = 0.25
+          }, {
+            id = "watches",
+            size = 0.25
+          } },
+        position = "left",
+        size = 40
+      }, {
+        elements = { {
+            id = "repl",
+            size = 1.0
+          -- }, {
+          --   id = "console",
+          --   size = 0.5
+          } },
+        position = "bottom",
+        size = 10
+      } },
+    } -- use default
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
+
+    local whichkey = require "which-key"
+    local keymap = {
+      d = {
+        name = "Debug",
+        s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
+        a = { "<cmd>lua require'dap'.restart()<cr>", "Restart" },
+        c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+        C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
+        d = { "<cmd>lua require'dap'.focus_frame()<cr>", "Focus current frame" },
+        i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
+        o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
+        u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
+        j = { "<cmd>lua require'dap'.up()<cr>", "Go up the stack frame" },
+        k = { "<cmd>lua require'dap'.down()<cr>", "Go down the stack frame" },
+        t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+        x = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
+        X = { "<cmd>lua require'dap'.clear_breakpoints()<cr>", "Clear breakpoints" },
+        -- f = { "<cmd>Telescope dap frames<cr>", "Telescope frames" },
+        -- b = { "<cmd>Telescope dap list_breakpoints<cr>", "Telescope list breakpoints" },
+        -- v = { "<cmd>lua require'telescope'.extensions.dap.variables{}<cr>", "Toggle Repl" },
+        r = { "<cmd>lua  require('dapui').float_element('repl', { width = 100, height = 20, enter = true, position = 'center' })<cr>", "Toggle Repl" },
+        w = { "<cmd>lua  require('dapui').float_element('watches', { width = 100, height = 20, enter = true, position = 'bottom' })<cr>", "Toggle Watch" },
+        h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
+        U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
+        e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+      },
+    }
+
+    whichkey.register(keymap, {
+      mode = "n",
+      prefix = "<leader>",
+      buffer = nil,
+      silent = true,
+      noremap = true,
+      nowait = false,
+    })
+
+    -- local keymap_v = {
+    --   name = "Debug",
+    --   e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+    -- }
+    -- whichkey.register(keymap_v, {
+    --   mode = "v",
+    --   prefix = "<leader>",
+    --   buffer = nil,
+    --   silent = true,
+    --   noremap = true,
+    --   nowait = false,
+    -- })
+  end,
+}
---
+++ lua/pde/debug/ruby.lua
@@ -0,0 +1,169 @@
+-- modified from suketa/nvim-dap-ruby
+-- allow for debugee to run externally
+-- include additional launch configurations
+local M = {}
+
+local function load_module(module_name)
+	local ok, module = pcall(require, module_name)
+	assert(ok, string.format("dap-ruby dependency error: %s not installed", module_name))
+	return module
+end
+
+local function mergeTables(defaults, custom)
+    local newTable = {}
+    for key, value in pairs(defaults) do
+        newTable[key] = value
+    end
+    for key, value in pairs(custom) do
+        newTable[key] = value
+    end
+    return newTable
+end
+
+local function splitAndAppend(str, array)
+    for word in str:gmatch("%S+") do
+        table.insert(array, word)
+    end
+end
+
+
+local function setup_ruby_adapter(dap)
+	dap.adapters.ruby = function(callback, config)
+		local handle
+		local stdout = vim.loop.new_pipe(false)
+		local pid_or_err
+		local waiting = config.waiting or 500
+		local args
+		local script
+		local rdbg
+
+		-- if we are attaching to an existing process, we don't need to start rdbg
+		if config.command ~= "attach" then
+			if config.current_line then
+				script = config.script .. ":" .. vim.fn.line(".")
+			else
+				script = config.script
+			end
+
+			if config.bundle == "bundle" then
+				args = { "-n", "--open", "--port", config.port, "-c", "--", "bundle", "exec" }
+			else
+				args = { "--open", "--port", config.port, "-c", "--" }
+			end
+
+			splitAndAppend(config.command, args)
+			table.insert(args, script)
+
+			local opts = {
+				stdio = { nil, stdout },
+				args = args,
+				detached = false,
+			}
+
+			if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+				rdbg = "rdbg.bat"
+			else
+				rdbg = "rdbg"
+			end
+
+			print("Starting rdbg: " .. rdbg .. " " .. table.concat(args, " "))
+			handle, pid_or_err = vim.loop.spawn(rdbg, opts, function(code)
+				handle:close()
+				if code ~= 0 then
+					assert(handle, "rdbg exited with code: " .. tostring(code))
+					print("rdbg exited with code", code)
+				end
+			end)
+
+			assert(handle, "Error running rdbg: " .. tostring(pid_or_err))
+		end
+
+		stdout:read_start(function(err, chunk)
+			assert(not err, err)
+			if chunk then
+				vim.schedule(function()
+					require("dap.repl").append(chunk)
+				end)
+			end
+		end)
+
+		-- Wait for rdbg to start
+		vim.defer_fn(function()
+			callback({ type = "server", host = config.server, port = config.port })
+		end, waiting)
+	end
+end
+
+local function setup_ruby_configuration(dap)
+	local default_config = {
+		type = "ruby",
+		request = "attach",
+		port = 38698,
+		server = "127.0.0.1",
+		options = {
+			source_filetype = "ruby",
+		},
+		localfs = true,
+		waiting = 1000,
+	}
+
+	local launch_configs = {
+		{
+			name = "attach to existing process",
+			command = "attach",
+			script = "not_used",
+		},
+		{
+			name = "rails",
+			bundle = "bundle",
+			command = "rails",
+			script = "s",
+		},
+		{
+			name = "rails test",
+			command = "rails test",
+			script = "${file}",
+			waiting = 10000,
+		},
+		{
+			name = "current file",
+			bundle = "",
+			command = "ruby",
+			script = "${file}",
+		},
+		{
+			name = "rspec current_file",
+			bundle = "bundle",
+			command = "rspec",
+			script = "${file}",
+		},
+		{
+			name = "rspec current_file:current_line",
+			bundle = "bundle",
+			command = "rspec",
+			script = "${file}",
+			current_line = true,
+		},
+		{
+			name = "rspec",
+			bundle = "bundle",
+			command = "rspec",
+			script = "./spec",
+		},
+
+	}
+
+	dap.configurations.ruby = {}
+	for _, launch_config in ipairs(launch_configs) do
+		local config = mergeTables(default_config, launch_config)
+		table.insert(dap.configurations.ruby, config)
+	end
+end
+
+function M.setup()
+	local dap = load_module("dap")
+	setup_ruby_adapter(dap)
+	setup_ruby_configuration(dap)
+end
+
+return M
