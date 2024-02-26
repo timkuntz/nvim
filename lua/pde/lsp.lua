51b9264de8a374b7c2ea64691143ca660110bbbb
Tim Kuntz <tim.kuntz@appfolio.com>
2024-02-25 22:04:43 -0600

    Fix Ruby-LSP integration and cleanup LSP config

--- lua/pde/lsp.lua
+++ lua/pde/lsp.lua
@@ -17,44 +17,52 @@ return {
     config = function()
       local lsp = require('lsp-zero').preset({})
 
-      lsp.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp.default_keymaps({buffer = bufnr})
-      end)
-
+      -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/lsp.md#commands
       vim.api.nvim_create_autocmd('LspAttach', {
 	desc = 'LSP actions',
 	callback = function(event)
-	  local opts = {buffer = event.buf}
-
-	  -- these will be buffer-local keybindings
-	  -- because they only work if you have an active language server
-
-	  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-	  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-	  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-	  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-	  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-	  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-	  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-	  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-	  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-	  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+	  local keymap = {
+	    g = {
+	      A = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "LSP Action" },
+	      F = { "<cmd>lua vim.lsp.buf.format({async = true})<cr>", "LSP Format" },
+	      D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "LSP Declaration" },
+	      K = { "<cmd>lua vim.lsp.buf.hover()<cr>", "LSP Hover" },
+	      R = { "<cmd>lua vim.lsp.buf.rename()<cr>", "LSP Rename" },
+	      d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "LSP Definition" },
+	      i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "LSP Implentation" },
+	      o = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "LSP Type Definition" },
+	      r = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "LSP References" },
+	      s = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "LSP Signature Help" },
+	    },
+	  }
+	  keymap['['] = {
+	    d = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "LSP Previous Diagnostic" }
+	  }
+	  keymap[']'] = {
+	    d = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "LSP Next Diagnostic" }
+	  }
+	  RegisterKeys(keymap, {
+	    prefix = "",
+	    buffer = event.buf
+	  })
 	end
       })
 
-      -- setup nvim-lspconfig
-      --    vim.opt.signcolumn = "yes"
-      --    vim.api.nvim_create_autocmd("FileType", {
-      --      pattern = "ruby",
-      --      callback = function()
-      -- vim.lsp.start {
-      --   name = "rubocop",
-      --   cmd = { "bundle", "exec", "rubocop", "--lsp" },
-      -- }
-      --      end,
-      --    })
+      -- lsp format on save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.rb",
+	callback = function()
+	  vim.lsp.buf.format()
+	end,
+      })
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lsp_config = require("lspconfig")
+
+      lsp_config.ruby_ls.setup {
+	capabilities = capabilities,
+	cmd = {os.getenv("HOME") .. "/.asdf/shims/ruby-lsp"},
+      }
 
       lsp.setup()
     end,
